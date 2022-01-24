119c4506368a53ab8996e6772a026da7c867eb86
ShyamBhalodia <shyambtridhyatech@gmail.com>
2022-01-24 18:02:40 +0530

    User Routes (Logged in user work with this apis)

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -5,13 +5,29 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getLoginUserDetails,
+  updatePassword,
+  updateLoggedInUserProfile,
 } = require('../controllers/UserController')
+const {isAuthenticatedUser} = require('../middlewere/Authentication')
 const router = express.Router()
 
+//Authentication
 router.route('/UserRegister').post(registerUser)
 router.route('/UserLogin').post(loginUser)
 router.route('/Password//UserForgotPassword').post(forgotPassword)
 router.route('/Password/UserResetPassword/:token').put(resetPassword)
 router.route('/UserLogout').get(logout)
+
+//User routes (For Logged in user)
+router
+  .route('/GetLoginUserDetails')
+  .get(isAuthenticatedUser, getLoginUserDetails)
+router
+  .route('/UpdateLoggedInUserPassword')
+  .patch(isAuthenticatedUser, updatePassword)
+router
+  .route('/UpdateLoggedInUserProfile')
+  .patch(isAuthenticatedUser, updateLoggedInUserProfile)
 
 module.exports = router
